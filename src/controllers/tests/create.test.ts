35c05d62914d410a5d9cc90376296be423a25649
ChandraShekar2001 <pvchandu07@gmail.com>
2024-05-01 20:21:32 +0530

    Initial commit

---
+++ src/controllers/tests/create.test.ts
@@ -0,0 +1,69 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { gigCreateSchema } from '@gig/validators/gig';
+import { BadRequestError } from '@chandrashekar2001/ecommerce-shared';
+import { Request, Response } from 'express';
+import { gigCreate } from '@gig/controllers/create';
+import * as helper from '@chandrashekar2001/ecommerce-shared';
+import * as gigService from '@gig/utils/gig.utils';
+import { authUserPayload, gigMockRequest, gigMockResponse, sellerGig } from '@gig/controllers/tests/mocks/gig.mock';
+
+jest.mock('@gig/utils/gig.utils');
+jest.mock('@chandrashekar2001/ecommerce-shared');
+jest.mock('@gig/validators/gig');
+jest.mock('@gig/elasticsearch');
+jest.mock('@elastic/elasticsearch');
+
+describe('Gig Controller', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('gig method', () => {
+    it('should throw an error for invalid schema data', () => {
+      const req: Request = gigMockRequest({}, sellerGig, authUserPayload) as unknown as Request;
+      const res: Response = gigMockResponse();
+      jest.spyOn(gigCreateSchema, 'validate').mockImplementation((): any =>
+        Promise.resolve({
+          error: {
+            name: 'ValidationError',
+            isJoi: true,
+            details: [{ message: 'This is an error message' }]
+          }
+        })
+      );
+
+      gigCreate(req, res).catch(() => {
+        expect(BadRequestError).toHaveBeenCalledWith('This is an error message', 'Create gig() method');
+      });
+    });
+
+    it('should throw file upload error', () => {
+      const req: Request = gigMockRequest({}, sellerGig, authUserPayload) as unknown as Request;
+      const res: Response = gigMockResponse();
+      jest.spyOn(gigCreateSchema, 'validate').mockImplementation((): any => Promise.resolve({error: {}}));
+      jest.spyOn(helper, 'uploads').mockImplementation((): any => Promise.resolve({ public_id: '' }));
+
+      gigCreate(req, res).catch(() => {
+        expect(BadRequestError).toHaveBeenCalledWith('File upload error. Try again.', 'Create gig() method');
+      });
+    });
+
+    it('should create a new gig and return the correct response', async () => {
+      const req: Request = gigMockRequest({}, sellerGig, authUserPayload) as unknown as Request;
+      const res: Response = gigMockResponse();
+      jest.spyOn(gigCreateSchema, 'validate').mockImplementation((): any => Promise.resolve({error: {}}));
+      jest.spyOn(helper, 'uploads').mockImplementation((): any => Promise.resolve({ public_id: '123456' }));
+      jest.spyOn(gigService, 'createGig').mockResolvedValue(sellerGig);
+
+      await gigCreate(req, res);
+      expect(res.status).toHaveBeenCalledWith(201);
+      expect(res.json).toHaveBeenCalledWith({
+        message: 'Gig created successfully.', gig: sellerGig
+      });
+    });
+  });
+});
